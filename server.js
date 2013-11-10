7611696ae05431af9923f8bd502c65e2c851c144
mgottein <mgottein@purdue.edu>
2013-11-10 01:13:53 -0600

    fixed directory issues with node

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var express = require("express");
 var app = express();
  
+ app.use(express.static(__dirname+'/public'));
+
  /* serves main page */
  app.get("/", function(req, res) {
-    res.sendfile('index.html')
+    res.sendfile('public/form.html')
  });
  
  var port = process.env.PORT || 80;
