877d94ffcd3718e53d21376164803031ff8251fa
mgottein <mgottein@purdue.edu>
2013-11-10 02:56:08 -0600

    Added twitter sign on basic code

--- server.js
+++ server.js
@@ -1,11 +1,37 @@
 var express = require("express");
 var app = express();
+var twitter = require("ntwitter");
+var mongojs = require('mongojs');
+var db = mongojs('db')
+var url = require('url');
+var collection = db.collection("test");
  
  app.use(express.static(__dirname+'/public'));
 
  /* serves main page */
  app.get("/", function(req, res) {
-    res.sendfile('public/form.html')
+    res.sendfile('public/index.html');
+ });
+
+ app.get("/form", function(req, res) {
+ 	res.sendfile('public/form.html');
+ })
+
+ app.get("/auth", function(req, res) {
+ 	var twit = new twitter({
+ 		consumer_key: "cjcr0YpkTnPreVobBkQ",
+ 		consumer_secret: "s9OFkPoL3FaU604fBhyekPsOxxmJaFMKKlMgAiCagZ4"
+ 	});
+ 	var path = url.parse(req.url, true);
+ 	var tlog = twit.login(path.pathname, "/form")(req, res);
+ })
+
+ app.get("/GetAnswer", function(req, res) {
+
+ });
+
+ app.get("/AskQuestion", function(req, res) {
+
  });
  
 
