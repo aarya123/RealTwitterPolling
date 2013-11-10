8efa43b202d13a69b50acf5ba6c4a8c69b78892a
mgottein <mgottein@purdue.edu>
2013-11-10 12:08:21 -0600

    cleaned up server

--- server.js
+++ server.js
@@ -56,135 +56,187 @@ app.get("/GetAnswer", function(req, res) {
 
 });
 
-//responses to questions are stored here
-var responses = {};
 //try to store only 1 user stream per user
 var streams = {};
 
+function Question(id_str, question, answers) {
+	this.id_str = id_str.toString();
+	this.rawQuestionText = question;
+	var hashTags = question.match(hashtagRegex);
+	if(hashTags && hashTags.length == 1) {
+		this.questionText = hashTags[0];
+	}
+	else {
+		throw "Invalid question " + question;
+	}
+	this.answers = {};
+	for(var i = 0; i < answers.length; ++i) {
+		if(answers[i].indexOf("#") != -1) {
+			throw "Invalid answer " + answer;
+		}
+		this.answers[answers[i]] = 0;
+	}
+	this.voters = {};
+}
+
+Question.prototype.isQuestionMatch = function(question) {
+	return this.id_str == question.id_str && this.questionText == question.questionText;
+}
+
+function Answer(rawAnswer) {
+	this.addr_id_str = rawAnswer.in_reply_to_user_id_str;
+	if(!this.addr_id_str) {
+		throw "Invalid answer " + rawAnswer + " no id";
+	}
+	this.id_str = rawAnswer.user.id_str;
+	var hashTags = rawAnswer.text.match(hashtagRegex);
+	if(hashTags && hashTags.length == 2) {
+		this.questionText = hashTags[0];
+		this.answerText = hashTags[1];
+		this.answerText = this.answerText.replace("#","");
+	}
+	else {
+		throw "Invalid answer " + rawAnswer + " no hash tag";
+	}
+}
+
+Question.prototype.isAnswerMatch = function(answer) {
+	return answer.addr_id_str == this.id_str && 
+	answer.questionText == this.questionText && 
+	this.answers[answer.answerText] >= 0 &&
+	!this.voters[answer.id_str];
+}
+
+Question.prototype.doAnswerMatch = function(answer) {
+	this.answers[answer.answerText]++;
+	this.voters[answer.id_str] = true;
+}
+
+Question.prototype.toString = function() {
+	return [this.id_str, this.questionText, this.answers].toString();
+}
+
+Answer.prototype.toString = function() {
+	return [this.addr_id_str, this.id_str, this.questionText, this.answerText].toString();
+}
+
+Question.prototype.getAnswerString = function() {
+	var answerString = "Results of " + this.questionText + ":";
+	for(var answer in this.answers) {
+		answerString += " #" + answer + " = " + this.answers[answer];
+	}
+	return answerString;
+}
+
+Question.prototype.getQuestionString = function() {
+	var questionString = "Poll: " + this.rawQuestionText;
+	questionString += " Answers:";
+	for(var answer in this.answers) {
+		questionString += " #" + answer;
+	}
+	return questionString;
+}
+
+function UserStream(twit) {
+	var self = this;
+	this.questions = [];
+	this.twit = twit;
+	twit.stream("user", {"replies": "all"},function(stream) {
+		self.stream = stream;
+		stream.on('data', function(data) {
+			try {
+				self.onData(data);
+			}
+			catch(e) {
+				console.log(e);
+			}
+		})
+	});
+	
+}
+
+UserStream.prototype.addQuestion = function(newQuestion) {
+	for(var i = 0; i < this.questions.length; ++i) {
+		if(newQuestion.isQuestionMatch(this.questions[i])) {
+			throw "found duplicate question " + newQuestion.toString();
+		}
+	}
+	this.questions.push(newQuestion);
+	var self = this;
+	setTimeout(function() {
+		console.log(newQuestion);
+		self.finishQuestion(newQuestion);
+	}, 60000);
+}
+
+UserStream.prototype.finishQuestion = function(question) {
+	var answerString = question.getAnswerString();
+	console.log(answerString);
+	this.twit.updateStatus(answerString, function(err, data) {});
+	this.questions = this.questions.splice(
+		this.questions.indexOf(question), 1
+	);
+	if(this.questions.length == 0) {
+		this.stream.destroy();
+		delete streams[this.user_id];
+	}
+}
+
+UserStream.prototype.onData = function(data) {
+	var newAnswer = new Answer(data);
+	console.log(newAnswer);
+	for(var i = 0; i < this.questions.length; ++i) {
+		if(this.questions[i].isAnswerMatch(newAnswer)) {
+			this.questions[i].doAnswerMatch(newAnswer);
+			console.log("match!");
+		}
+		else {
+			console.log("no match!");
+		}
+	}
+}
+
+
 //convenient regex for hashtags
 var hashtagRegex = /[#]+[A-Za-z0-9-_]+/g
+
 
 app.post("/askQuestion", function(req, res) {
 	var twit = new twitter({
 		"consumer_key": consumer_key,
 		"consumer_secret": consumer_secret
 	});
-	twit.gatekeeper()(req,res,function(){
+	if(twit.gatekeeper()(req,res,function(){
 		console.log(req.body);
 		var req_cookie = twit.cookie(req);
 		var user_id = req_cookie.user_id;
 		//not secure at all, but whatever
 		twit.options.access_token_key = req_cookie.access_token_key;
 		twit.options.access_token_secret = req_cookie.access_token_secret;
-		//make sure each user has their own array inside the response array
-		if(!responses[user_id]) {
-			responses[user_id] = {};
+		var newQuestion;
+		try {
+			newQuestion = new Question(req_cookie.user_id,
+				req.body.question,
+				req.body.answers);
 		}
-		//TODO: reorganize logic so it isn't inside verifyCredentials
-		twit.verifyCredentials(function (err, data) {
-			res.writeHead(200, {'Content-Type': 'text/plain'});
-			if(err) {
-				res.write("FAIL");
-			}
-			else {
-				//We are only OK if there is exactly 1 hash tag in the question
-				var matches = req.body.question.match(hashtagRegex);
-				if(matches && matches.length == 1) {
-					res.write("OK");
-					var questionMatch = matches[0];
-					//initial question status
-					twit.updateStatus(req.body.question + 
-						" #" + req.body.answers.join(' , #'), function(err, data) {
-							if(!err) {
-								//Make the specific question be its own array inside the user sub array
-								responses[user_id][questionMatch] = {};
-								//initialize all answers to 0
-								var question = responses[user_id][questionMatch];
-								for(var i = 0; i < req.body.answers.length; ++i) {
-									question[req.body.answers[i]] = 0;
-								}
-							}
-						});
-					//if we don't already have a active user stream for this user, make a new one
-					if(!streams[user_id]) {
-						stream = twit.stream("user", function(stream) {
-							//cleanup clears the stream from the stream array and outputs the final tally of answers
-							function cleanup() {
-								stream.destroy()
-								delete streams[user_id];
-								var question = responses[user_id][questionMatch];
-								//# of votes is # of people who answered + # of answers - # of answers
-								var votes = Object.keys(question).length - req.body.answers.length;
-								var status;
-								if(votes != 0) {
-									//TODO: make this look better
-									status = "Results for " + req.body.question + " -";
-									for(var i = 0; i < req.body.answers.length; ++i) {
-										var answer = req.body.answers[i];
-										console.log(answer, question[answer]);
-										status += " #" + answer + " = " + 
-										(((question[answer] / votes) * 10000) / 100) + "%";
-									}
-								}
-								else {
-									status = "No votes for poll " + req.body.question + "!";
-								}
-								twit.updateStatus(status, function(err, data) {
-
-								});
-							}
-							stream.on('data', function(data) {
-								console.log(data.text);
-								//if this user is talking to our user, check for a potential question being answered
-								if(data.in_reply_to_user_id_str == user_id) {
-									var matches = data.text.match(hashtagRegex);
-									if(matches && matches.length == 2) {
-										//question should be the 1st match and answer should be the second
-										var question = matches[0], answer = matches[1];
-										answer = answer.replace("#","");
-										//if this question was asked by the user
-										if(responses[user_id][question]) {
-											var curQuestion = responses[user_id][question];
-											//if the answer is valid
-											if(curQuestion[answer] >= 0) {
-												//if this user hasn't already voted
-												if(!curQuestion[data.user.id]) {
-													curQuestion[answer]++;
-													curQuestion[data.user.id] = true;
-												}
-												else {
-													twit.updateStatus("@" + data.user.screen_name +
-														" you already voted in my poll", function(err, data) {}); 
-												}
-											}
-											else {
-												twit.updateStatus("@" + data.user.screen_name + 
-													" I couldn't understand " +
-													"your reply to my poll. Try voting again", function(err, data) {});
-											}
-										}
-										console.log(responses[user_id][question]);
-									}
-								}
-							});
-stream.on('error', function(data, code) {
-	console.log("error", data, code);
-	cleanup();
-});
-							//stream.on('destroy', cleanup);
-							//stream.on('end', cleanup);
-							//TODO: questions should be able to last longer than 1:40
-							setTimeout(cleanup, 100000);
-						});
-}
-else {
-	res.write("FAIL");
-}
-}
-}
-res.end();
-});
-});
+		catch(e) {
+			console.log(e);
+			return false;
+		}
+		if(!streams[user_id]) {
+			streams[user_id] = new UserStream(twit);
+		}
+		streams[user_id].addQuestion(newQuestion);
+		twit.updateStatus(newQuestion.getQuestionString()
+			, function(err, data) {});
+		return true;
+	})) {
+		res.write("OK");
+	}
+	else {
+		res.write("FAIL");
+	}
+	res.end();
 });
 
 
